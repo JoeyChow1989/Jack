2e50eaaa0e300a897abb7362a6b1b73ef6c10392
zzy <zzycj0258@126.com>
2016-04-08 13:11:36 +0800

    --------2016/4/8-----------------

---
+++ TuRingRobot/src/main/java/com/turing/www/demo/activity/PersonActivity.java
@@ -0,0 +1,17 @@
+package com.turing.www.demo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.turing.www.demo.R;
+
+public class PersonActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+}
---
+++ TuRingRobot/src/main/java/com/turing/www/demo/appliaction/MyApplication.java
@@ -0,0 +1,28 @@
+package com.turing.www.demo.appliaction;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Administrator on 2015/11/25.
+ */
+public class MyApplication extends Application
+{
+
+    //请求队列
+    public static RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+
+    public static RequestQueue getHttpQueues()
+    {
+        return mRequestQueue;
+    }
+}
