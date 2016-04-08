4334de55972d80a4bb5f60a3a43e32fe2ac78c60
zzy <zzycj0258@126.com>
2016-04-08 13:11:48 +0800

    --------2016/4/8-----------------

---
+++ TuRingRobot/src/main/java/com/turing/www/demo/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.turing.www.demo.util;
+
+/**
+ * Created by Administrator on 2015/12/11.
+ */
+public class HttpUtil
+{
+    //API地址
+    public static final String URL = "http://www.tuling123.com/openapi/api";
+
+    //API KEY
+    public static final String API_KEY = "b6d17e1b7195bf5ba9c97aa9ff07e9dc";
+
+}
